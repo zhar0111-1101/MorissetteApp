28f1fbca69287425e406eec43626d7b742cabe92
zhar0111-1101 <57603494+zhar0111-1101@users.noreply.github.com>
2020-05-06 11:58:43 -0400

    Update browseInventory.js

--- frontend/src/Components/browseInventory.js
+++ frontend/src/Components/browseInventory.js
@@ -1,6 +1,48 @@
 import React from 'react'
+import axios from 'axios';
 
+class BrowseInventory extends Component {
+    state = {
+        query: '',
+    }
 
+    componentDidMount = () => {
+        this.getCarInfo();
+    }
+
+    getCarInfo = () => {
+        axios.get('/api')
+        .then((response) => {
+            const data = response.data;
+            this.setState({query: this.search.value});
+            console.log('Cars recevied');
+        })
+        .catch(() => {
+            alert('Error retrieving cars');
+        });
+
+    }
+
+    handleInputChange = () => {
+        this.setState({
+            query: this.search.value
+        })
+    }
+
+    render () {
+        return (
+        <form>
+            <input
+                placeholder="Search for..."
+                ref={input => this.search = input}
+                onChange={this.handleInputChange}
+            />
+            <p>{this.state.query}</p>
+        </form>
+        )
+    }
+}
+/*
 const BrowseInventory = () => {
     return ( 
         <div>   
@@ -8,5 +50,5 @@ const BrowseInventory = () => {
         </div>
     )
 }
-
+*/
 export default BrowseInventory;
